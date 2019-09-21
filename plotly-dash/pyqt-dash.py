39d899f4d59fa48e027abd9cc5dcb85758b0ee9c
NelisW <neliswillers@gmail.com>
2019-09-21 16:44:16 +0200

    interim commit, working towards reading plot definitions from xcel file Now separated the data read from the display code.

--- plotly-dash/pyqt-dash.py
+++ plotly-dash/pyqt-dash.py
@@ -3,24 +3,19 @@ https://github.com/plotly/dash-recipes
 https://github.com/plotly/dash-recipes/blob/master/multiple-hover-data.py
 https://plot.ly/python/subplots/
 
-
 conda config --add channels conda-forge
 conda search dash-daq --channel conda-forge
-
 
 conda install dash
 conda install dash-html-components
 conda install dash-core-components
 conda install dash-table
 conda install dash-daq
-
-
 """
 
 import sys
 import threading
-
-# import json
+import pandas as pd
 
 from PyQt5 import QtWidgets
 import PyQt5.QtCore as QtCore
@@ -30,14 +25,10 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-
 # import plotly.graph_objs as go
 
 
-import pandas as pd
-
-
-def makedccgraph(gid, height, title, data):
+def makeDccGraph(gid, height, title, data):
     dccgraph = dcc.Graph(
                 id=gid,
                 figure={'layout':{'title':title},'data':data},
@@ -46,18 +37,47 @@ def makedccgraph(gid, height, title, data):
     return dccgraph
 
 
-def run_dash(dataSource, mode):
+def makeHTMLPage(pagetitle,pageheader,lstgraphs):
+    # build a page for display
+    page = html.Div(
+        children=[
+            html.H1(children=pagetitle),
+            html.Div([dcc.Markdown(children=pageheader)]),
+            # following is a list of dcc.Graph(() graphs
+            *lstgraphs
+        ]
+    )
+    return page
 
+
+def run_dash(pageLayout,mode):
     # start a dash app, which also starts a Flask server
     app = dash.Dash()
-
     # override security restrictions: allow the serving of local pages
     app.css.config.serve_locally = True
     app.scripts.config.serve_locally = True
+    app.layout = pageLayout
+    # app.css.append_css({
+    #     'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
+    # })
+    app.run_server(debug=False, port=8050)
 
-    filename = "data/tp05j2a.rgeo"
-    df = pd.read_csv(filename, sep="\s+", index_col=None)
-    print(df.columns)
+class WebViewer(QtWebEngineWidgets.QWebEngineView):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        page = QtWebEngineWidgets.QWebEnginePage(self)
+        self.setPage(page)
+        self.setUrl(QtCore.QUrl("http://127.0.0.1:8050"))
+
+
+#####################################################
+def makePageFromCVS(configfile,datafile):
+
+
+
+    # read the data file
+    df = pd.read_csv(datafile, sep="\s+", index_col=None)
+    # print(df.columns)
     # print(df.head())
 
     grpData1 = [
@@ -123,56 +143,33 @@ def run_dash(dataSource, mode):
         },
     ]
 
-    demograph1 = makedccgraph(gid="position", height=300, title="position", data=grpData1)
-    demograph2 = makedccgraph(gid="speed", height=600, title="speed", data=grpData2)
-    demograph3 = makedccgraph(gid="speedxy", height=600, title="speed", data=grpData3)
+    demograph1 = makeDccGraph(gid="position", height=300, title="position", data=grpData1)
+    demograph2 = makeDccGraph(gid="speed", height=600, title="speed", data=grpData2)
+    demograph3 = makeDccGraph(gid="speedxy", height=600, title="speed", data=grpData3)
 
     lstgraphs = [demograph1,demograph2,demograph3]
 
-    def makePage(pagetitle,pageheader,lstgraphs):
-        # build a page for display
-        layout = html.Div(
-            children=[
-                html.H1(children=pagetitle),
-                html.Div([dcc.Markdown(children=pageheader)]),
-                # following is a list of dcc.Graph(() graphs
-                *lstgraphs
-            ]
-        )
-        return layout
-
     pageheader = """Attempting to use *Dash* for plotting in PyQt5."""
     pagetitle='Dash Experiments'
-    app.layout = makePage(pagetitle,pageheader,lstgraphs)
 
-    # app.css.append_css({
-    #     'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
-    # })
+    page = makeHTMLPage(pagetitle,pageheader,lstgraphs)
 
-    app.run_server(debug=False, port=8050)
+    return page
 
-
-class WebViewer(QtWebEngineWidgets.QWebEngineView):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        page = QtWebEngineWidgets.QWebEnginePage(self)
-        self.setPage(page)
-        self.setUrl(QtCore.QUrl("http://127.0.0.1:8050"))
-
-
+##########################################
 #
 
 if __name__ == "__main__":
 
     sys.argv.append("--disable-web-security")
 
-    dataSource = None  # fill in later
-    mode = None  # fill in later
+    datafile = "data/tp05j2a.rgeo"
+    configfile = None
+    pageLayout = makePageFromCVS(configfile,datafile) 
+    mode = None # fill in later, we need this to make the thread work
 
-    threading.Thread(target=run_dash, args=(dataSource, mode), daemon=True).start()
-
+    threading.Thread(target=run_dash, args=(pageLayout,mode), daemon=True).start()
     app = QtWidgets.QApplication(sys.argv)
-
     main_widget = QtWidgets.QWidget(None)
     window_layout = QtWidgets.QVBoxLayout(main_widget)
     # window_layout.addWidget(WebViewer(main_widget))
